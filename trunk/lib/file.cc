dbf4e57d9e40219678ca5b5d4d2f69f0145ed89d
ka0ttic <ka0ttic@a6ae5f90-adf1-0310-8726-dab32267b732>
2005-04-05 16:23:05 +0000

    cosmetic changes

--- trunk/lib/file.cc
+++ trunk/lib/file.cc
@@ -29,78 +29,9 @@
 
 #include "util.hh"
 
-/*
- * Is the specified path a directory?
- */
-bool
-util::is_dir(const char *path)
-{
-    struct stat s;
-    if (stat(path, &s) != 0)
-	return false;
-    return util::is_dir(s);
-}
-
-bool util::is_dir(const std::string &path) { return is_dir(path.c_str()); }
-bool util::is_dir(const struct stat &s) { return S_ISDIR(s.st_mode); }
-
-/*
- * Is the specified path a regular file?
- */
-
-bool
-util::is_file(const char *path)
-{
-    struct stat s;
-    if (stat(path, &s) != 0)
-	return false;
-    return util::is_file(s);
-}
-
-bool util::is_file(const std::string &path) { return is_file(path.c_str()); }
-bool util::is_file(const struct stat &s) { return S_ISREG(s.st_mode); }
-
-const char *
-util::basename(const char *path)
-{
-    std::string result(path);
-    std::string::size_type pos;
-
-    /* chop all trailing /'s */
-    while (result[result.length() - 1] == '/' and result.length() > 1)
-	result.erase(result.length() - 1);
-
-    if ((pos = result.rfind('/')) != std::string::npos)
-	result = result.substr(pos + 1);
-
-    return ( result.empty() ? "/" : result.c_str() );
-}
-
-const char *
-util::basename(std::string const &path) { return util::basename(path.c_str()); }
-
-const char *
-util::dirname(const char *path)
-{
-    std::string result(path);
-    std::string::size_type pos;
-
-    /* chop all trailing /'s */
-    while (result[result.length() - 1] == '/' and result.length() > 1)
-	result.erase(result.length() - 1);
-
-    if ((pos = result.rfind('/')) != std::string::npos)
-	result = result.substr(0, pos);
-
-    return ( result.empty() ? "/" : result.c_str() );
-}
-
-const char *
-util::dirname(std::string const &path) { return util::dirname(path.c_str()); }
-
-/*
- * fileobject_T
- */
+/**************************
+ * fileobject_T           *
+ **************************/
 
 util::fileobject_T::fileobject_T(const char *n, util::type_T t) : _name(n), _type(t)
 {
@@ -115,9 +46,9 @@ util::fileobject_T::fileobject_T(const std::string &n, util::type_T t)
         throw util::errno_E("stat: " + _name);
 }
 
-/*
- * dir_T
- */
+/**************************
+ * dir_T                  *
+ **************************/
 
 void
 util::dir_T::read(bool recurse)
@@ -175,5 +106,75 @@ util::dir_T::~dir_T()
     for (iterator i = _contents.begin() ; i != _contents.end() ; ++i)
         delete *i;
 }
+
+/******************************************
+ * general purpose file-related functions *
+ ******************************************/
+
+bool
+util::is_dir(const char *path)
+{
+    struct stat s;
+    if (stat(path, &s) != 0)
+	return false;
+    return S_ISDIR(s.st_mode);
+}
+
+bool util::is_dir(const std::string &path) { return is_dir(path.c_str()); }
+bool util::is_dir(const struct stat &s) { return S_ISDIR(s.st_mode); }
+
+/*
+ * Is the specified path a regular file?
+ */
+
+bool
+util::is_file(const char *path)
+{
+    struct stat s;
+    if (stat(path, &s) != 0)
+	return false;
+    return S_ISREG(s.st_mode);
+}
+
+bool util::is_file(const std::string &path) { return is_file(path.c_str()); }
+bool util::is_file(const struct stat &s) { return S_ISREG(s.st_mode); }
+
+const char *
+util::basename(const char *path)
+{
+    std::string result(path);
+    std::string::size_type pos;
+
+    /* chop all trailing /'s */
+    while (result[result.length() - 1] == '/' and result.length() > 1)
+	result.erase(result.length() - 1);
+
+    if ((pos = result.rfind('/')) != std::string::npos)
+	result = result.substr(pos + 1);
+
+    return ( result.empty() ? "/" : result.c_str() );
+}
+
+const char *
+util::basename(std::string const &path) { return util::basename(path.c_str()); }
+
+const char *
+util::dirname(const char *path)
+{
+    std::string result(path);
+    std::string::size_type pos;
+
+    /* chop all trailing /'s */
+    while (result[result.length() - 1] == '/' and result.length() > 1)
+	result.erase(result.length() - 1);
+
+    if ((pos = result.rfind('/')) != std::string::npos)
+	result = result.substr(0, pos);
+
+    return ( result.empty() ? "/" : result.c_str() );
+}
+
+const char *
+util::dirname(std::string const &path) { return util::dirname(path.c_str()); }
 
 /* vim: set tw=80 sw=4 et : */
