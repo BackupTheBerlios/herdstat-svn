8661805eb866a5d736a17a891216b10a1594538c
ka0ttic <ka0ttic@a6ae5f90-adf1-0310-8726-dab32267b732>
2005-06-10 16:55:33 +0000

    Tagged 1.1.1_rc8 release.

---
+++ tags/release-1.1.1_rc8/src/metacache.cc
@@ -0,0 +1,299 @@
+/*
+ * herdstat -- src/metacache.cc
+ * $Id$
+ * Copyright (c) 2005 Aaron Walker <ka0ttic@gentoo.org>
+ *
+ * This file is part of herdstat.
+ *
+ * herdstat is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * herdstat is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * herdstat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 325, Boston, MA  02111-1257  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <algorithm>
+#include <functional>
+
+#include "pkgcache.hh"
+#include "metadata_xml.hh"
+#include "metacache.hh"
+
+#define METACACHE               LOCALSTATEDIR"/metacache"
+#define METACACHE_EXPIRE        259200 /* 3 days */
+#define METACACHE_RESERVE       8650
+
+metacache_T::metacache_T(const util::string &portdir)
+    : util::cache_T<value_type>(METACACHE), _portdir(portdir) { }
+
+/*
+ * Is the cache valid?
+ */
+
+bool
+metacache_T::valid() const
+{
+    struct stat s;
+    bool valid = false;
+
+    const util::string expire(optget("metacache.expire", util::string));
+
+    if (stat(METACACHE, &s) == 0)
+    {
+        if (expire == "lastsync")
+        {
+            const util::string path(this->_portdir + "/metadata/timestamp");
+            bool timestamp = util::is_file(path);
+            bool lastsync  = util::is_file(LASTSYNC);
+
+            if (timestamp and lastsync)
+            {
+                util::file_T t(path), l(LASTSYNC);
+
+                if (optget("debug", bool))
+                {
+                    debug_msg("Checking timestamps...");
+                    t.dump(std::cout);
+                    l.dump(std::cout);
+                }
+
+                valid = (t == l);
+                
+                if (not valid)
+                {
+                    debug_msg("timestamps don't match ; replacing lastsync");
+                    t.close(); l.close();
+                    util::copy_file(path, LASTSYNC);
+                }
+            }
+            else if (lastsync)
+            {
+                unlink(LASTSYNC);
+                valid = ((std::time(NULL) - s.st_mtime) < METACACHE_EXPIRE);
+            }
+            else if (timestamp)
+                util::copy_file(path, LASTSYNC);
+            else
+                valid = ((std::time(NULL) - s.st_mtime) < METACACHE_EXPIRE);
+        }
+        else
+            valid = ((std::time(NULL) - s.st_mtime) <
+                    std::strtol(expire.c_str(), NULL, 10));
+
+        /* only valid if size > 0 */
+        if (valid)
+            valid = (s.st_size > 0);
+    }
+
+    debug_msg("metadata cache is valid? %d", valid);
+    return valid;
+}
+
+/*
+ * Find and parse every metadata.xml in the tree,
+ * filling our container with data.
+ */
+
+void
+metacache_T::fill()
+{
+    const bool status = not optget("quiet", bool) and not optget("debug", bool);
+    {
+        util::progress_T progress;
+        pkgcache_T pkgcache(this->_portdir);
+        debug_msg("pkgcache.size() == %d", pkgcache.size());
+
+        if (status)
+        {
+            *(optget("outstream", std::ostream *))
+                << "Generating metadata.xml cache: ";
+            progress.start(pkgcache.size());
+        }
+
+        /* for each pkg */
+        pkgcache_T::iterator p;
+        for (p = pkgcache.begin() ; p != pkgcache.end() ; ++p)
+        {
+            if (status)
+                ++progress;
+
+            util::path_T metadata(this->_portdir + "/" + (*p) + "/metadata.xml");
+            if (metadata.exists())
+            {
+                /* parse it */
+                const metadata_xml_T m(metadata);
+                this->push_back(m.data(this->_portdir));
+            }
+        }
+    }
+
+    if (status)
+        *(optget("outstream", std::ostream *)) << std::endl;
+}
+
+/*
+ * Load cache from disk.  If a vector is given, only load lines
+ * whose package is in the vector.
+ */
+
+void
+metacache_T::load()
+{
+    if (not util::is_file(METACACHE))
+        return;
+
+    try
+    {
+        const util::vars_T cache(METACACHE);
+
+        this->_portdir = cache["portdir"];
+        if (this->_portdir.empty())
+            throw metacache_parse_E();
+
+        /* reserve to prevent tons of reallocations */
+        if (cache["size"].empty() or cache["size"] == "0")
+            this->reserve(METACACHE_RESERVE);
+        else
+            this->reserve(std::atoi(cache["size"].c_str()));
+
+        util::vars_T::const_iterator i;
+        for (i = cache.begin() ; i != cache.end() ; ++i)
+        {
+            /* not a category/package, so skip it */
+            if (i->first.find('/') == util::string::npos)
+                continue;
+
+            util::string str;
+            metadata_T meta(this->_portdir,
+                this->_portdir + "/" + i->first + "/metadata.xml");
+
+            std::vector<util::string> parts = i->second.split(':', true);
+            if (parts.empty())
+                throw metacache_parse_E();
+
+            /* get herds */
+            str = parts.front();
+            parts.erase(parts.begin());
+            meta.herds = str.split(',');
+
+            /* get devs */
+            if (not parts.empty())
+            {
+                str = parts.front();
+                parts.erase(parts.begin());
+                if (not str.empty())
+                    meta.devs = str.split(',');
+            }
+
+            /* get longdesc */
+            if (not parts.empty())
+            {
+                str = parts.front();
+                parts.erase(parts.begin());
+
+                /* longdesc contains a ':', so reconstruct it */
+                while (not parts.empty())
+                {
+                    str += ":" + parts.front();
+                    parts.erase(parts.begin());
+                }
+
+                meta.longdesc = str;
+            }
+
+            this->push_back(meta);
+        }
+    }
+    catch (const metacache_parse_E)
+    {
+        std::cerr << "Error parsing " << METACACHE << std::endl;
+        throw;
+    }
+}
+
+/*
+ * Dump cache to disk.
+ */
+
+void
+metacache_T::dump()
+{
+    std::ofstream f(METACACHE);
+    if (not f.is_open())
+        throw util::bad_fileobject_E(METACACHE);
+
+    f << "# Automatically generated by " << PACKAGE << "-" << VERSION
+        << std::endl;
+    f << "portdir=" << this->_portdir << std::endl;
+    f << "size=" << this->size() << std::endl;
+
+    /* for each metadata_T object */
+    for (iterator ci = this->begin() ; ci != this->end() ; ++ci)
+    {
+        /*
+         * format is the form of:
+         *   cat/pkg=herd1,herd2:dev1,dev2:longdesc
+         */
+
+        util::string str;
+        std::size_t n;
+
+        f << ci->pkg << "=";
+
+        /* herds */
+        metadata_T::herds_type::iterator h;
+        for (h = ci->herds.begin(), n = 1 ; h != ci->herds.end() ; ++h, ++n)
+        {
+            str += (*h);
+            if (n != ci->herds.size())
+                str += ",";
+        }
+
+        f << str << ":";
+        
+        /* developers */
+        metadata_T::herd_type::iterator d;
+        for (d = ci->devs.begin(), n = 1, str.clear() ; d != ci->devs.end() ; 
+            ++d, ++n)
+        {
+            str += d->first;
+            if (n != ci->devs.size())
+                str += ",";
+        }
+
+        f << str << ":";
+
+        /* longdesc */
+        if (ci->longdesc.empty())
+            f << std::endl;
+        else
+        {
+#ifdef UNICODE
+            try
+            {
+                f << util::tidy_whitespace(ci->longdesc) << std::endl;
+            }
+            catch (const Glib::ConvertError)
+            {
+                f << std::endl;
+            }
+#else /* UNICODE */
+            f << util::tidy_whitespace(ci->longdesc) << std::endl;
+#endif /* UNICODE */
+        }
+    }
+}
+
+/* vim: set tw=80 sw=4 et : */
---
+++ tags/release-1.1.1_rc8/src/pkgcache.cc
@@ -0,0 +1,184 @@
+/*
+ * herdstat -- src/pkgcache.cc
+ * $Id$
+ * Copyright (c) 2005 Aaron Walker <ka0ttic@gentoo.org>
+ *
+ * This file is part of herdstat.
+ *
+ * herdstat is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * herdstat is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * herdstat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 325, Boston, MA  02111-1257  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <algorithm>
+#include <iterator>
+
+#include "pkgcache.hh"
+
+#define PKGCACHE                    LOCALSTATEDIR"/pkgcache"
+#define PKGCACHE_EXPIRE             259200 /* 3 days */ 
+
+pkgcache_T::pkgcache_T() : util::cache_T<value_type>(PKGCACHE) { }
+
+pkgcache_T::pkgcache_T(const util::string &portdir)
+    : util::cache_T<value_type>(PKGCACHE), _portdir(portdir)
+{
+    util::cache_T<value_type>::init();
+}
+
+void
+pkgcache_T::init(const util::string &portdir)
+{
+    this->_portdir.assign(portdir);
+    util::cache_T<value_type>::init();
+}
+
+/*
+ * pkg cache is valid?
+ */
+
+bool
+pkgcache_T::valid() const
+{
+    struct stat s;
+    bool valid = false;
+
+    const util::string expire(optget("metacache.expire", util::string));
+
+    if (stat(PKGCACHE, &s) == 0)
+    {
+        if (expire == "lastsync")
+        {
+            const util::string path(this->_portdir + "/metadata/timestamp");
+            bool timestamp = util::is_file(path);
+            bool lastsync  = util::is_file(LASTSYNC);
+
+            if (timestamp and lastsync)
+            {
+                util::file_T t(path), l(LASTSYNC);
+                valid = (t == l);
+                if (not valid)
+                {
+                    debug_msg("timestamp != lastsync ; replacing lastsync file.");
+                    t.close(); l.close();
+                    util::copy_file(path, LASTSYNC);
+                }
+            }
+            else if (lastsync)
+            {
+                unlink(LASTSYNC);
+                valid = ((std::time(NULL) - s.st_mtime) < PKGCACHE_EXPIRE);
+            }
+            else if (timestamp)
+                util::copy_file(path, LASTSYNC);
+            else
+                valid = ((std::time(NULL) - s.st_mtime) < PKGCACHE_EXPIRE);
+        }
+        else
+            valid = ((std::time(NULL) - s.st_mtime) <
+                    std::strtol(expire.c_str(), NULL, 10));
+
+        /* only valid if size > 0 */
+        if (valid)
+            valid = (s.st_size > 0);
+    }
+
+    debug_msg("pkgcache is valid? %d", valid);
+    return valid;
+}
+
+/*
+ * Traverse PORTDIR looking for all packages.
+ */
+
+void
+pkgcache_T::fill()
+{
+    util::timer_T timer;
+
+    if (optget("timer", bool))
+        timer.start();
+
+    const portage::categories_T categories(this->_portdir,
+        optget("qa", bool));
+
+    std::vector<util::string>::iterator i;
+    std::vector<util::string> dirs = 
+        optget("portage.config", portage::config_T).overlays();
+    dirs.insert(dirs.begin(), this->_portdir);
+
+    /* for each category */
+    portage::categories_T::const_iterator c;
+    for (c = categories.begin() ; c != categories.end() ; ++c)
+    {
+        for (i = dirs.begin() ; i != dirs.end() ; ++i)
+        {
+            const util::path_T cat(*i + "/" + (*c));
+            if (not cat.exists())
+                continue;
+
+            /* for each directory in category */
+            const util::dir_T category(cat);
+            util::dir_T::const_iterator d;
+            for (d = category.begin() ; d != category.end() ; ++d)
+            {
+                util::string pkg(util::sprintf("%s/%s", c->c_str(), d->basename()));
+                if ((*i == this->_portdir) or 
+                    (std::find(this->begin(), this->end(), pkg) == this->end()))
+                    this->push_back(pkg);
+            }
+        }
+    }
+
+    if (optget("timer", bool))
+    {
+        timer.stop();
+        debug_msg("Took %ldms to fill package cache.", timer.elapsed());
+    }
+}
+
+/*
+ * Load package cache from disk.
+ */
+
+void
+pkgcache_T::load()
+{
+    std::ifstream stream(PKGCACHE);
+    if (not stream.is_open())
+        throw util::bad_fileobject_E(PKGCACHE);
+
+    std::copy(std::istream_iterator<util::string>(stream),
+        std::istream_iterator<util::string>(), std::back_inserter(*this));
+}
+
+/*
+ * Dump package cache to disk.
+ */
+
+void
+pkgcache_T::dump()
+{
+    std::ofstream stream(PKGCACHE);
+    if (not stream.is_open())
+        throw util::bad_fileobject_E(PKGCACHE);
+
+    std::copy(this->begin(), this->end(),
+        std::ostream_iterator<util::string>(stream, "\n"));
+}
+
+/* vim: set tw=80 sw=4 et : */
