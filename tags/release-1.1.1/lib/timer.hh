548e323f35b8a54001555643ffb079a26bb1513f
ka0ttic <ka0ttic@a6ae5f90-adf1-0310-8726-dab32267b732>
2005-06-19 14:11:50 +0000

    Tagged 1.1.1 release.

---
+++ tags/release-1.1.1/lib/timer.hh
@@ -0,0 +1,74 @@
+/*
+ * herdstat -- lib/timer.hh
+ * $Id$
+ * Copyright (c) 2005 Aaron Walker <ka0ttic@gentoo.org>
+ *
+ * This file is part of herdstat.
+ *
+ * herdstat is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * herdstat is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * herdstat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 325, Boston, MA  02111-1257  USA
+ */
+
+#ifndef HAVE_TIMER_HH
+#define HAVE_TIMER_HH 1
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <cstddef>
+#include <sys/time.h>
+
+namespace util
+{
+    class timer_T
+    {
+        public:
+            typedef long size_type;
+            typedef struct timeval time_type;
+
+            timer_T() : _elapsed(0), _running(false) { }
+
+            void start()
+	    {
+	        gettimeofday(&(this->_begin), NULL);
+                this->_running = true;
+	    }
+
+	    void stop()
+	    {
+	        gettimeofday(&(this->_end), NULL);
+                size_type tmp(this->_elapsed);
+	        this->_elapsed = this->_end.tv_sec - this->_begin.tv_sec;
+	        this->_elapsed *= 1000;
+	        this->_elapsed +=
+                    (this->_end.tv_usec - this->_begin.tv_usec) / 1000;
+                this->_elapsed += tmp;
+                this->_running = false;
+	    }
+
+            bool is_running() const { return this->_running; }
+            size_type elapsed() const { return this->_elapsed; }
+            void clear() { this->_elapsed = 0; }
+
+        private:
+	    time_type _begin, _end;
+	    size_type _elapsed;
+            bool _running;
+    };
+}
+
+#endif
+
+/* vim: set tw=80 sw=4 et : */
---
+++ tags/release-1.1.1/src/pkgquery.hh
@@ -0,0 +1,60 @@
+/*
+ * herdstat -- src/pkgquery.hh
+ * $Id$
+ * Copyright (c) 2005 Aaron Walker <ka0ttic@gentoo.org>
+ *
+ * This file is part of herdstat.
+ *
+ * herdstat is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * herdstat is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * herdstat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 325, Boston, MA  02111-1257  USA
+ */
+
+#ifndef HAVE_PKGQUERY_HH
+#define HAVE_PKGQUERY_HH 1
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <map>
+#include "common.hh"
+#include "herds_xml.hh"
+
+/*
+ * Container for package query data.
+ */
+
+enum query_type { QUERYTYPE_DEV, QUERYTYPE_HERD };
+
+class pkgQuery_T : public std::map<util::string, util::string>
+{
+    public:
+        pkgQuery_T(const util::string &n, const util::string &w = "",
+            bool dev = false) : info(n), query(n), with(w),
+                                portdir(optget("portdir", util::string)),
+                                type(dev? QUERYTYPE_DEV : QUERYTYPE_HERD) { }
+
+        void dump(std::ostream &) const;
+        bool operator== (const pkgQuery_T &) const;
+        std::vector<util::string> pkgs() const;
+
+        herds_xml_T::devinfo_T info;
+        util::string query, with, portdir;
+        std::time_t date;
+        query_type type;
+};
+
+#endif
+
+/* vim: set tw=80 sw=4 et : */
