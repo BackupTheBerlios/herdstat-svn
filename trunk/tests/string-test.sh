e3ec7599c55bb543a6dadc93ca9581e9e318c79e
ka0ttic <ka0ttic@a6ae5f90-adf1-0310-8726-dab32267b732>
2005-09-18 11:48:40 +0000

    Set srcdir if it's null.

--- trunk/tests/string-test.sh
+++ trunk/tests/string-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source common.sh || exit 1
 run_test "$(get_caller ${0})" "string utility functions" \
-    "${srcdir:-.}/src/string-test" "" || exit 1
+    "${srcdir}/src/string-test" "" || exit 1
 indent
